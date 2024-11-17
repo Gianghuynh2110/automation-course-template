a3bb747d2649a5480a34a120a4abf5ab46c62d49
Giang Huynh <kimgiang2110@gmail.com>
2024-11-17 16:48:46 +0700

    Update bài 16

--- src/test/java/com/Bai16_ShoppingCartTest.java
+++ src/test/java/com/Bai16_ShoppingCartTest.java
@@ -11,23 +11,40 @@ import com.utils.BasicTest;
 
 public class Bai16_ShoppingCartTest extends BasicTest {
 
-    
-    @Test(priority = 1)
+  
+    @Test()
     public void shoppingCartTestsuccess() throws Exception {
         // Launch website
         String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
+        WebElement email = driver.findElement(By.xpath("//input[@id='username']"));
+        email.sendKeys("kimgiang2110@gmail.com");
+        WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
+        password.sendKeys("Giang@123");
+        WebElement loginButton = driver.findElement(By.xpath("//button [@name='login']"));
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement exitButton = driver.findElement(By.xpath("//li[contains(@class,'logout')]"));
+        Assert.assertTrue(exitButton.isDisplayed());
+
         WebElement searchField = driver.findElement(By.xpath("(//input[@id='s'])[1]"));
         searchField.sendKeys("merc");
 
-        
-        WebElement passwordRegister = driver.findElement(By.xpath("//input[@autocomplete='new-password']"));
-        passwordRegister.sendKeys("Giang@123");
-        WebElement registerButton = driver.findElement(By.xpath("//button[@name='register']"));
-        registerButton.click();
-        WebElement errorMessage = driver.findElement(By.xpath("//label[@for='reg_password']"));
-        Assert.assertTrue(errorMessage.isDisplayed());
+        WebElement searchResult = driver.findElement(By.xpath("//a[contains(text(),'Bơm nước xe')]"));
+        searchResult.click();
+
+        WebElement origindropdown = driver.findElement(By.xpath("//select[@id='pa_xuat-xu']"));
+        origindropdown.click();
+
+        WebElement originvalue = driver.findElement(By.xpath("//option[contains(text(), 'England')]"));
+        originvalue.click();
+    
+        WebElement addButton = driver.findElement(By.xpath("//button[contains(text(),'Thêm vào giỏ hàng')]"));
+        addButton.click();
+
+        WebElement successmessage = driver.findElement(By.xpath("//a[@class='button wc-forward']"));
+        Assert.assertTrue(successmessage.isDisplayed());
 
 
     }
